e528a6cdc1f8aa5b1d7ad3280d703478423c7abe
galihpramono <gpramono5@gmail.com>
2023-12-09 14:31:14 +0700

    menambahkan file project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hamburger Menu di Klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.targer) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
